c53a5ff41d1b75d0e4ff24cbae096ee6524e060f
Kristoffer Vestergaard <kristoffer@krve.io>
2017-01-04 20:42:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hyper-mac-controls! [![npm-version][npm-badge]][npm-link]
+
+A plugin that makes the close, minimize and maximize buttons look like the macOS window controls. The plugin is currently only testing on linux, but this should work in Windows aswell.
+
+![hyperterm-mac-controls][screenshot]
+
+### Install
+
+1. Install using `hpm install hyperterm-mac-controls`
+2. Reload or restart hyperterm.
+
+### License
+
+MIT © [krve][author]
+
+[screenshot]: https://cloud.githubusercontent.com/assets/5139119/21655977/766986e0-d2bc-11e6-8182-fd48c55c4416.png
+[npm-badge]:  https://img.shields.io/npm/v/hyperterm-mac-controls.svg?style=flat-square
+[npm-link]:   https://www.npmjs.com/package/hyperterm-mac-controls
+[author]:     https://github.com/krve
---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const remote = require('electron').remote;
+
+exports.decorateConfig = (config) => {
+    return Object.assign({}, config, {
+        css: `
+            ${config.css || ''}
+            .header_windowHeader {
+                height: 22px;
+                left: 57px;
+                width: calc(100% - 56px);
+            }
+            .header_windowControls {
+                display: none;
+            }
+            .header_appTitle {
+                margin-left: -56px;
+            }
+            .mac_header {
+                position: fixed;
+                top: 0;
+                left: 0;
+                height: 22px;
+                width: 56px;
+            }
+            .mac_actions {
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                top: 0;
+            }
+            .mac_header .mac_close,
+            .mac_header .mac_minimize,
+            .mac_header .mac_maximize {
+                width: 12px;
+                height: 12px;
+                border-radius: 50%;
+                position: absolute;
+                top: 5px;
+                background-position: -6px;
+            }
+            .mac_header .mac_close {
+                background-color: #f25056;
+                background-image: url('${__dirname}/icons/close.svg');
+                left: 5px;
+            }
+            .mac_header .mac_close:hover {
+                background-image: url('${__dirname}/icons/close_hover.svg');
+            }
+            .mac_header .mac_minimize {
+                background-color: #fac536;
+                background-image: url('${__dirname}/icons/minimize.svg');
+                left: 23px;
+            }
+            .mac_header .mac_minimize:hover {
+                background-image: url('${__dirname}/icons/minimize_hover.svg');
+            }
+            .mac_header .mac_maximize {
+                background-color: #39ea49;
+                background-image: url('${__dirname}/icons/maximize.svg');
+                left: 40px;
+            }
+            .mac_header .mac_maximize:hover {
+                background-image: url('${__dirname}/icons/maximize_hover.svg');
+            }
+        `
+    })
+};
+
+exports.decorateHeader = (Hyper, { React }) => {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                window: null,
+                maximized: false
+            }
+            this.closeApp = this.closeApp.bind(this);
+            this.minimizeApp = this.minimizeApp.bind(this);
+            this.maximizeApp = this.maximizeApp.bind(this);
+        }
+        closeApp() {
+            this.state.window.close();
+        }
+        minimizeApp() {
+            this.state.window.minimize();
+            this.state.maximized = false;
+        }
+        maximizeApp() {
+            if (this.state.maximized == true) {
+                this.state.window.unmaximize();
+                this.state.maximized = false;
+            } else {
+                this.state.window.maximize();
+                this.state.maximized = true;
+            }
+        }
+        render() {
+            return (
+                React.createElement(Hyper, Object.assign({}, this.props, {
+                    customChildren: React.createElement('div', { className: 'mac_header' },
+                        React.createElement('div', { className: 'mac_actions' },
+                            React.createElement('span', { className: 'mac_close', onClick: this.closeApp }),
+                            React.createElement('span', { className: 'mac_minimize', onClick: this.minimizeApp }),
+                            React.createElement('span', { className: 'mac_maximize', onClick: this.maximizeApp })
+                        )
+                    )
+                }))
+            )
+        }
+        componentDidMount() {
+            this.state.window = remote.getCurrentWindow();
+        }
+    };
+};
